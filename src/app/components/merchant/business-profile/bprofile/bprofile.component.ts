e8bcbc1326d769b537a9c03114920f4158fbda96
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-03-21 07:58:35 +0530

    21 mar bprofile data binding

--- src/app/components/merchant/business-profile/bprofile/bprofile.component.ts
+++ src/app/components/merchant/business-profile/bprofile/bprofile.component.ts
@@ -6,13 +6,16 @@ export interface Business {
   description: string;
   email: string;
   contact_no: string;
-  address: string;
+  address_line1: string;
+  address_line2: string;
+  postal_code: string;
   location: string;
   isverified: boolean;
   isopened: boolean;
   feature_img: string;
   logo: string;
 }
+
 @Component({
   selector: 'app-bprofile',
   templateUrl: './bprofile.component.html',
@@ -26,15 +29,26 @@ export class BprofileComponent implements OnInit {
   //create new profile mode
   addnew = false;
 
-  //business verification
-  isverified = false;
-
   //enabling ctomization
   isowner = true;
 
   //bprofile data binding
-  business: Business = [
-    {id: '1', title: 'Chiran \'s Business', }
+  business: Business[] = [
+    {
+      id: '1',
+      title: 'Chiran \'s Business',
+      description: 'sample description of chiran s business',
+      email: 'chiran@cbusiness.com',
+      contact_no: '0711112233',
+      address_line1: 'Main Street',
+      address_line2: 'Dalugama, Kelaniya',
+      postal_code: '11300',
+      location: 'https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed', 
+      isverified: false,
+      isopened: true,
+      feature_img: './assets/images/back/bprofile.jpg',
+      logo: './assets/images/merchant/business.jpg'
+    }
   ];
 
   constructor() { }

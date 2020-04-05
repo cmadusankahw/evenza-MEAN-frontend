a38845035f9ceadbc61050d0f9af9f0c47887c62
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-04-05 14:32:19 +0530

    frontend 04 apr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ import { SignupSelectComponent } from './modules/auth/signup-select/signup-selec
 import { SignupMerchantComponent } from './modules/auth/signup-merchant/signup-merchant.component';
 import { DashboardComponent } from './modules/serviceprovider/serviceprovider-dash/dashboard/dashboard.component';
 import { BprofileComponent } from './modules/serviceprovider/business-profile/bprofile/bprofile.component';
+import { DashAppointmentsComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-appointments/dash-appointments.component';
+import { DashBusinessProfileComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-business-profile/dash-business-profile.component';
+import { DashBookingsComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-bookings/dash-bookings.component';
+import { DashCalendarComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-calendar/dash-calendar.component';
+import { DashReportsComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-reports/dash-reports.component';
+import { DashProfileComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-profile/dash-profile.component';
+import { DashHomeComponent } from './modules/serviceprovider/serviceprovider-dash/pages/dash-home/dash-home.component';
+import { ContactUsComponent } from './modules/home/contact-us/contact-us.component';
 
 
 const routes: Routes = [
@@ -14,8 +22,19 @@ const routes: Routes = [
   {path : 'register', component : SignupSelectComponent},
   {path : 'register/common', component : SignupComponent},
   {path : 'register/merchant', component : SignupMerchantComponent},
-  {path : 'sp/dash', component : DashboardComponent},
+  {path : 'sp/dash',
+   component : DashboardComponent ,
+   children : [
+      {path : '' , component : DashHomeComponent},
+      {path : 'bprofile' , component : DashBusinessProfileComponent},
+      {path : 'bookings' , component : DashBookingsComponent},
+      {path : 'appoints' , component : DashAppointmentsComponent},
+      {path : 'calendar' , component : DashCalendarComponent},
+      {path : 'reports' , component : DashReportsComponent},
+      {path : 'profile' , component : DashProfileComponent},
+    ]},
   {path : 'sp/bprofile', component : BprofileComponent},
+  {path : 'contactus', component : ContactUsComponent},
   {path : '', component : HomeComponent}
 ];
 
--- src/app/modules/event/event-news/event-news.component.ts
+++ src/app/modules/event/event-news/event-news.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Event {
+  event_id: string;
+  event_title: string;
+  type: string;
+  description: string;
+  organizer: string;
+  date: string;
+  no_of_days: number;
+  start_time: string;
+  end_time: string;
+  feature_img: string;
+  features: any;
+}
+
 @Component({
   selector: 'app-event-news',
   templateUrl: './event-news.component.html',
   styleUrls: ['./event-news.component.scss']
 })
 export class EventNewsComponent implements OnInit {
+
+  events: Event [] = [
+    {
+      event_id: 'E-01', event_title: 'Saman Wedding', type: 'closed', description: 'Saman & Kamala Wedding Ceremony',
+      organizer: ' Saman Kumara', date: '04/07/2020', no_of_days: 1, start_time: '09:00', end_time: '16:00',
+      feature_img: './assets/images/events/1.jpg', features: ['Lunch', 'Evening Tea']
+    },
+    {
+      event_id: 'E-02', event_title: 'AI Hackathon', type: 'closed', description: 'Global AI Hackathon',
+      organizer: ' Chiran HW', date: '04/22/2020', no_of_days: 1, start_time: '16:00', end_time: '00:00',
+      feature_img: './assets/images/events/2.jpg', features: ['T-shirt', 'Lunch', 'Swags']
+    }
+  ];
 
   constructor() { }
 
